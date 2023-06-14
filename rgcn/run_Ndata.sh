3f682d2bd45826580a78cae1ddf33627d5af6b18
Tinc0ist <34474781+tincochan@users.noreply.github.com>
2023-06-14 10:02:26 +0800

    Create run_Ndata.sh

---
+++ rgcn/run_Ndata.sh
@@ -0,0 +1,1 @@
+python3 train.py --device 1 --epoch 40 --data real-n --sampling xgb --train_from 20140101 --test_from 20170101 --test_length 365 --valid_length 90 --initial_inspection_rate 99.9 --lr 0.05 --pos_weight 50
