65e4899953d5e6f936fa510ef2e9ba26c8bb7ea2
Tinc0ist <34474781+tincochan@users.noreply.github.com>
2023-06-14 10:02:18 +0800

    Create run_Tdata.sh

---
+++ rgcn/run_Tdata.sh
@@ -0,0 +1,2 @@
+python3 train.py --device 1 --epoch 5 --data real-t --sampling xgb --train_from 20170101 --test_from 20190101 --test_length 365 --valid_length 90 --initial_inspection_rate 99.9 --lr 0.004 --pos_weight 20
+
