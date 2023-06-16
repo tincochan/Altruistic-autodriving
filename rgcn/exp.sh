47d6503204e7538db0c51bb7179b35a5bdbbf9a2
Tinc0ist <34474781+tincochan@users.noreply.github.com>
2023-06-16 15:05:37 +0800

    Create exp.sh

---
+++ rgcn/exp.sh
@@ -0,0 +1,10 @@
+sh run_Ndata.sh ; 
+sh run_Cdata.sh ; 
+sh run_Tdata.sh ;
+# sh ablation/run_Tdata_nogbdt.sh ; 
+# sh ablation/run_Ndata_nogbdt.sh ; 
+# sh ablation/run_Cdata_nogbdt.sh ;
+for i in 1 2 10 20
+do
+    
+done
