632e686037e76d96d43a7109dd53898705d2fcfb
Tinc0ist <34474781+tincochan@users.noreply.github.com>
2023-06-23 20:53:15 +0800

    Create run_Cdata.sh

---
+++ graph_sage/run_Cdata.sh
@@ -0,0 +1,1 @@
+python3 train.py --dim 32 --device 1 --epoch 40 --data real-t --sampling xgb --train_from 20160101 --test_from 20190101 --test_length 365 --valid_length 90 --initial_inspection_rate 5 --lr 0.05 --pos_weight 20
